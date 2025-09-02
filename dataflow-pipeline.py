db3ad7b1113911c129eb01fc88cc54c1dfe54957
mssugith <39847756+mssugith@users.noreply.github.com>
2025-09-02 23:07:08 +0530

    Update dataflow-pipeline.py

--- dataflow-pipeline.py
+++ dataflow-pipeline.py
@@ -1,31 +1,49 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 
-class ProcessHealthcareData(beam.DoFn):
-    def process(self, element):
-        # Add data cleaning/transformation logic
-        yield element
+
+class DataflowOptions(PipelineOptions):
+    @classmethod
+    def _add_argparse_args(cls, parser):
+        parser.add_value_provider_argument("--input", type=str, help="GCS input file")
+        parser.add_value_provider_argument("--output", type=str, help="BigQuery table")
+
+
+def transform_data(element):
+    fields = element.split(",")
+    return {
+        "Chest_Pain": fields[0],
+        "Shortness_of_Breath": fields[1],
+        "Age": int(fields[2]) if fields[2].isdigit() else None,
+        "Heart_Risk": fields[3],
+    }
+
 
 def run():
-    options = PipelineOptions(
-        project='healthcare-analytics-sugith',
-        runner='DataflowRunner',
-        region='asia-south1-a',
-        zone='asia-south1-a',
-        temp_location='gs://healthcare-raw-data-healthcare-analytics-sugith/temp /'
+    pipeline_options = PipelineOptions(
+        save_main_session=True,
+        runner="DataflowRunner",  # local test
+        project="healthcare-analytics-sugith",
+        region="asia-south1",   # region not zone
+        temp_location="gs://healthcare-processed-data-healthcare-analytics-sugith/temp/",
+        staging_location="gs://healthcare-processed-data-healthcare-analytics-sugith/staging/",
     )
-  
-    with beam.Pipeline(options=options) as p:
-        (p
-         | 'ReadFromGCS' >> beam.io.ReadFromText('gs://healthcare-raw-data-healthcare-analytics-sugith/Hdataset/heart_disease_risk_dataset_earlymed2.csv')
-         | 'ParseCSV' >> beam.Map(lambda line: line.split(','))
-         | 'ProcessData' >> beam.ParDo(ProcessHealthcareData())
-         | 'WriteToBigQuery' >> beam.io.WriteToBigQuery(
-             'healthcare-analytics-sugith:patient_details.patients_factors_2',
-             schema='Chest_Pain: BINARY, Shortness_of_Breath: BINARY,	Fatigue: BINARY,	Palpitations: BINARY,	Dizziness: BINARY,	Swelling: BINARY, Age: INT, Heart_Risk: BINARY ',
-             create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
-             write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND)
+
+    options = pipeline_options.view_as(DataflowOptions)
+
+    with beam.Pipeline(options=pipeline_options) as p:
+        (
+            p
+            | "Read from GCS" >> beam.io.ReadFromText(options.input, skip_header_lines=1)
+            | "Transform" >> beam.Map(transform_data)
+            | "Write to BigQuery" >> beam.io.WriteToBigQuery(
+                table=options.output,
+                schema="Chest_Pain:STRING, Shortness_of_Breath:STRING, Age:INTEGER, Heart_Risk:STRING",
+                write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
+                create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+            )
         )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run()
